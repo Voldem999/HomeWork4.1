67a5300d182dc24a62639a28cf637a240aeea893
Ilya201091 <ZiK201091@ya.ru>
2022-11-17 23:39:28 +0700

    Initial commit

---
+++ scripts.sql
@@ -0,0 +1,23 @@
+select *
+from students
+where age between 10 and 20;
+
+select name
+from students;
+
+select *
+from students
+where name like '%О%';
+
+select *
+from students
+where age < id;
+
+select *
+from students
+order by age;
+
+select *
+from students
+order by age desc;
+
